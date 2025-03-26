7341f25419acf4a88e23e3b56d7ddd1858babb5a
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2025-03-26 11:06:56 +0100

    feat: simplify mapping-selection: remove rename, delete. add edit.

--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-selection/mapping-selection.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-selection/mapping-selection.component.ts
@@ -3,15 +3,12 @@ import { FormControl, Validators } from '@angular/forms';
 import { FieldMapping } from '@geonature/modules/imports/models/mapping.model';
 
 import { FieldMappingService } from '@geonature/modules/imports/services/mappings/field-mapping.service';
-import { ConfigService } from '@geonature/services/config.service';
-import { finalize, skip } from 'rxjs/operators';
+import { skip } from 'rxjs/operators';
 import { ImportProcessService } from '../../import-process.service';
 import { Cruved, toBooleanCruved } from '@geonature/modules/imports/models/cruved.model';
 import { CruvedStoreService } from '@geonature_common/service/cruved-store.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ImportDataService } from '@geonature/modules/imports/services/data.service';
-import { CommonService } from '@geonature_common/service/common.service';
 import { Subscription } from 'rxjs';
+import { ConfigService } from '@geonature/services/config.service';
 
 @Component({
   selector: 'pnx-mapping-selection',
@@ -27,19 +24,13 @@ export class MappingSelectionComponent implements OnInit {
 
   private fieldMappingSub: Subscription;
 
-  public renameMappingFormVisible: boolean = false;
-  public deleteMappingFormVisible: boolean = false;
-
   @ViewChild('deleteConfirmModal') deleteConfirmModal;
 
   constructor(
     private _fm: FieldMappingService,
-    private config: ConfigService,
     private _importProcessService: ImportProcessService,
-    private _importDataService: ImportDataService,
     public cruvedStore: CruvedStoreService,
-    private _modalService: NgbModal,
-    private _commonService: CommonService
+    private _config: ConfigService
   ) {
     this.cruved = toBooleanCruved(this.cruvedStore.cruved.IMPORT.module_objects.MAPPING.cruved);
     this.fieldMappingForm = this._fm.mappingSelectionFormControl;
@@ -72,81 +63,14 @@ export class MappingSelectionComponent implements OnInit {
    * @param {FieldMapping} mapping - the selected mapping
    */
   onNewMappingSelected(mapping: FieldMapping = null): void {
-    // this.hideCreateOrRenameMappingForm();
     this._fm.currentFieldMapping.next(mapping);
-  }
-
-  // hideCreateOrRenameMappingForm() {
-  // this.createOrRenameMappingForm.reset();
-  // }
-  toggleRenameMappingForm(valueToggle: boolean = null) {
-    if (!this.fieldMappingForm.value?.label) {
-      this._commonService.translateToaster('error', 'Import.FieldMapping.SelectMapping');
-    } else {
-      this.renameMappingFormVisible =
-        valueToggle !== null ? valueToggle : !this.renameMappingFormVisible;
-      if (this.renameMappingFormVisible) {
-        this.createOrRenameMappingForm.setValue(this.fieldMappingForm.value.label);
-      }
-    }
   }
 
   isMappingSelected(): boolean {
     return this.fieldMappingForm.value != null;
   }
 
-  openDeleteModal() {
-    if (!this.fieldMappingForm.value?.label) {
-      this._commonService.translateToaster('error', 'Import.FieldMapping.SelectMapping');
-    } else {
-      this.toggleRenameMappingForm(false);
-      this.toggleDeleteModal(true);
-      this._modalService.open(this.deleteConfirmModal);
-    }
-  }
-
-  toggleDeleteModal(valueToggle: boolean = null) {
-    this.deleteMappingFormVisible =
-      valueToggle !== null ? valueToggle : !this.deleteMappingFormVisible;
-  }
-
-  renameMapping(): void {
-    this._importDataService
-      .renameFieldMapping(this.fieldMappingForm.value.id, this.createOrRenameMappingForm.value)
-      .pipe(
-        finalize(() => {
-          this.renameMappingFormVisible = false;
-        })
-      )
-      .subscribe((mapping: FieldMapping) => {
-        let index = this.userFieldMappings.findIndex((m: FieldMapping) => m.id == mapping.id);
-        this.fieldMappingForm.setValue(mapping);
-        this.userFieldMappings[index] = mapping;
-      });
-  }
-
-  deleteMapping() {
-    // this.spinner = true;
-    let mapping_id = this.fieldMappingForm.value.id;
-    this._importDataService
-      .deleteFieldMapping(mapping_id)
-      .pipe()
-      .subscribe(
-        () => {
-          this._commonService.translateToaster('success', 'Import.FieldMapping.MappingDeleted', {
-            label: this.fieldMappingForm.value.label,
-          });
-          this.fieldMappingForm.setValue(null, { emitEvent: false });
-          this.userFieldMappings = this.userFieldMappings
-            .filter((mapping) => {
-              return mapping.id !== mapping_id;
-            })
-            .sort((a, b) => a.label.localeCompare(b.label));
-          // this.spinner = false;
-        },
-        () => {
-          // this.spinner = false;
-        }
-      );
+  get editionURLinAdmin(): string {
+    return this._config.API_ENDPOINT + '/admin/fieldmapping/';
   }
 }
